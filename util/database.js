80eb0ad05b5fd78596294b5dae3e719350151b17
MojeshChundi <112922928+MojeshChundi@users.noreply.github.com>
2023-04-20 01:09:29 +0530

    sql schema

--- util/database.js
+++ util/database.js
@@ -1,10 +1,18 @@
-const mysql = require("mysql2");
-
-const pool = mysql.createPool({
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("node-complete", "root", "Bujji@123", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: "Bujji@123",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
+
+// const mysql = require("mysql2");
+
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   database: "node-complete",
+//   password: "Bujji@123",
+// });
+
+// module.exports = pool.promise();
